b33b983c1ea649e8b3590b926ede3ac5a0b840d8
Kevin Wang <kaybinwang@gmail.com>
2023-03-18 03:56:54 -0400

    add missing init files

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "A library for serializing and deserializing DHCP packets"
+LONG_DESCRIPTION = (
+    "A package that makes it easier to work with DHCP packets by representing them as "
+    "immutable, schematized Python objects."
+)
+
+setup(
+    name="dhcp-packer",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Kevin Wang",
+    author_email="kaybinwang@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords="dhcp",
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
